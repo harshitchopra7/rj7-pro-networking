d8f7ca8e9c91b06ad6ddcd116a1d5eb35aceb045
harshitchopra7 <chopraharshit2000@gmail.com>
2023-01-22 12:22:13 +0530

    add posts

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -4,6 +4,7 @@ import FeedCard from "./FeedCard";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
 
   async function getPosts() {
     const response = await fetch("https://dummyjson.com/posts");
@@ -16,12 +17,28 @@ function Feed() {
     getPosts();
   }, []);
 
+  function addPosts() {
+    const copyArray = posts;
+    copyArray.unshift({
+      userId: 10,
+      name: "Harshit",
+      tags: ["HTML", "CSS"],
+      body: input,
+    });
+    setInput("");
+  }
+
   return (
     <div className="feed_container">
       <div className="feed_add">
         <div className="feed_add_input">
-          <input />
-          <button>Add</button>
+          <input
+            type="text"
+            value={input}
+            placeholder="Write something..."
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <button onClick={addPosts}>Add</button>
         </div>
 
         <button className="feed_add_photo">Add Photo</button>
@@ -30,7 +47,7 @@ function Feed() {
       {posts?.map((value) => (
         <FeedCard
           image={value.userId}
-          name={value.userId}
+          name={value.name}
           tags={value.tags}
           message={value.body}
         />
