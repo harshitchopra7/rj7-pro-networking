5969fa4b9a3a574f2f46bdf5246cbf47c2cc4827
harshitchopra7 <chopraharshit2000@gmail.com>
2023-01-30 09:24:00 +0530

    dynamic route

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/Homepage/Homepage";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import Profile from "./pages/Profile/Profile";
+import FeedDescriptionPage from "./pages/FeedDescriptionPage/FeedDescriptionPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/about-us" element={<AboutUs />} />
           <Route exact path="/profile" element={<Profile />} />
+          <Route
+            exact
+            path="/feed-description/:id"
+            element={<FeedDescriptionPage />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/FeedDescriptionPage/FeedDescriptionPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FeedDesription from "../../components/FeedDescription/FeedDesription";
+
+function FeedDescriptionPage() {
+  return (
+    <div>
+      <FeedDesription />
+    </div>
+  );
+}
+
+export default FeedDescriptionPage;
